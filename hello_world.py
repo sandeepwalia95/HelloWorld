dd3d066f604e2b6ba0eecf399fda0022175b3957
sandeepwalia <sandeepwalia95@hotmail.com>
2020-04-08 23:09:09 +0100

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,32 @@
+from random import choice
+
+
+class Person:
+
+    x = 0
+    
+    def __init__(self):
+        self.id = Person.x
+        Person.x += 2
+
+    def getId(self):
+        return self.id
+
+
+
+
+def main():
+    
+    people = [
+        Person(),
+        Person(),
+        Person()
+    ]
+    
+    for p in people:
+        num = p.getId()
+        print(p.id)
+
+
+if __name__ == '__main__':
+    main()
